e9a21e5bae44502affcf19fe0d0da37a92467455
inesvar <inesvarhol@gmail.com>
2022-09-22 18:09:09 +0200

    Exercice 1 tiré du cours de compilation - traduction d'une boucle C en assembleur

---
+++ sept_assembleurARM/Cours-compil-ex1.s
@@ -0,0 +1,51 @@
+/* TRADUCTION DU CODE :
+
+uint32_t a;
+...
+for (uint8_t i=0; i<=a; i++) {
+        g();
+}
+*/
+
+
+    ldr r0, [var] //declaration de a
+
+    mov r1, #0
+loop :
+    bl g
+    add r1, r1, #1
+    and r1, r1, #255 //masque pour imiter un uint8_t
+    ldr r2, [r0] //on relit la valeur de a
+    cmp r1, r2
+    bls loop //si inferieur ou egal
+    bx lr
+
+var :
+    .word a
+
+
+/* 
+
+Meme code MAIS i n'est plus un uint8_t mais un int
+
+*/
+
+//Les int sont generalement sur 4 octets
+//la seule difference avec le code precedent est qu'on n'a plus besoin du masque
+
+
+    ldr r0, [var] //declaration de a
+
+    mov r1, #0
+loop :
+    bl g
+    add r1, r1, #1
+    ldr r2, [r0] //on relit la valeur de a
+    cmp r1, r2
+    bls loop //si inferieur ou egal
+    bx lr
+
+var :
+    .word a
+
+
