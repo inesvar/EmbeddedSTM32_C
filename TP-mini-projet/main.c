1ca46920bf089254cd55c748e7bf15688890e4dc
inesvar <inesvarhol@gmail.com>
2022-09-30 16:39:04 +0200

    Finalisation du main

--- TP-mini-projet/main.c
+++ TP-mini-projet/main.c
@@ -1,21 +1,26 @@
 #include <stdint.h>
 
-
-int fibo(int n); //n-ieme nombre de la suite de Fibonacci
+int fibo(int n);
 
 int number_in_bss;
 
-int main(int argc, const char * argv[]) {
-    number_in_bss = 2;
-    return number_in_bss;
+int main(int argc, const char *argv[])
+{
+    return fibo(8);
 }
 
-int fibo(int n) {
+
+
+int fibo(int n)
+{
     /* Cette fonction calcule le n-ieme nombre de la suite de Fibonacci
     pour n un entier strictement positif */
-    if (n < 3) {
+    if (n < 3)
+    {
         return 1;
-    } else {
-        return fibo(n-1)+fibo(n-2);
-    } 
+    }
+    else
+    {
+        return fibo(n - 1) + fibo(n - 2);
+    }
 }
