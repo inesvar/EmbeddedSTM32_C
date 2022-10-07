ef381f6179dd66024f96a74a0442a0b9e80e488f
inesvar <inesvarhol@gmail.com>
2022-10-07 12:10:46 +0200

    led verte ne s'allume plus - pas d'écriture dans le BSRR ?

--- TP-mini-projet/led.c
+++ TP-mini-projet/led.c
@@ -1,42 +1,33 @@
 #include <stdint.h>
 #include "led.h"
-
-// adresse de RCC : 0x4002 1000
-// RCC_AHB2ENR : offset 0x4C
-#define RCC_AHB2ENR (*(volatile uint32_t *)0x4002104C)
-
-// adresse du peripherique B : 0x4800 0400
-#define GPIO_B_MODER (*(volatile uint32_t *)0x48000400)
-#define GPIO_B_BSRR (*(volatile uint32_t *)0x48000418)
-
-// adresse du peripherique C :
-#define GPIO_C_MODER (*(volatile uint32_t *)0x48000800)
-#define GPIO_C_BSRR (*(volatile uint32_t *)0x48000818)
+//#include "stm32l475xx.h"
+#include "stm32l4xx.h"
 
 void led_init()
 {
-    // activer horloge
+    // activer horloge dans RCC_AHB2ENR
     // horloge GPIOB : bit 1 à 1
     // horloge GPIOC : bit 2 à 1
-    RCC_AHB2ENR = (RCC_AHB2ENR & ~(3 << 1)) | (3 << 1);
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
 
     // PB14 en mode sortie :
     // on écrira 01 dans les bits [29:28] de GPIO_B_MODER
-    GPIO_B_MODER = (GPIO_B_MODER & ~(3 << 28)) | (1 << 28);
+    GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODE14_Msk) | GPIO_MODER_MODE14_0;
 
     // PC9 en sortie :
     // 01 dans les bits [19:18] de GPIO_C_MODER
-    GPIO_C_MODER = (GPIO_C_MODER & ~(3 << 18)) | (1 << 18);
+    GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_0;
 }
 
 void led_g_on()
 {
-    GPIO_B_BSRR = 1 << 14;
+    GPIOB->MODER |= GPIO_BSRR_BS14;
 }
 
 void led_g_off()
 {
-    GPIO_B_BSRR = 1 << 30;
+    GPIOB->MODER |= GPIO_BSRR_BR14;
 }
 
 void led(enum State state)
@@ -45,16 +36,18 @@ void led(enum State state)
     {
     case LED_OFF:
         //passer GPIOC en mode entree eteint les deux leds
-        GPIO_C_MODER = (GPIO_C_MODER & ~(3 << 18));
+        // PC9 en entree :
+        // 10 dans les bits [19:18] de GPIO_C_MODER
+        GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_1;
         break;
     case LED_YELLOW:
-        GPIO_C_MODER = (GPIO_C_MODER & ~(3 << 18)) | (1 << 18);
-        GPIO_C_BSRR = 1 << 9;
+        GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9;
+        GPIOC->MODER |= GPIO_BSRR_BS9;
         break;
 
     case LED_BLUE:
-        GPIO_C_MODER = (GPIO_C_MODER & ~(3 << 18)) | (1 << 18);
-        GPIO_C_BSRR = 1 << 25;
+        GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9;
+        GPIOC->MODER |= GPIO_BSRR_BR9;
         break;
     }
 }
